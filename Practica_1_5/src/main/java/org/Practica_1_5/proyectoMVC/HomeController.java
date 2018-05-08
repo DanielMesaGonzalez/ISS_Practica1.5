9213f3320bf58f74cc71c134412a696068709faa
Salvador <std00002@red.ujaen.es>
2018-05-08 20:57:10 +0200

    Sesion Carrito terminada.

--- Practica_1_5/src/main/java/org/Practica_1_5/proyectoMVC/HomeController.java
+++ Practica_1_5/src/main/java/org/Practica_1_5/proyectoMVC/HomeController.java
@@ -8,7 +8,9 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Locale;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -35,7 +37,7 @@ public class HomeController {
 	@Autowired
 	private ProductoDAOInterface daop;
 	
-	private List<ProductosDTO> itemsCarrito = new ArrayList<ProductosDTO>();
+	//private List<ProductosDTO> SesionCarrito = new ArrayList<ProductosDTO>();
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -47,7 +49,7 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/Registro", method = RequestMethod.POST)
-	public String registropost(HttpServletRequest request,Locale locale, Model model) {
+	public String registropost(HttpServletRequest request,Locale locale, Model model,HttpServletResponse response) {
 		HttpSession sesion = request.getSession();
 	    String nombre= (String)request.getParameter("username");
 	    String apellido= (String)request.getParameter("surname");
@@ -57,6 +59,12 @@ public class HomeController {
 	    sesion.setAttribute("nusuario", user);
 	    request.setAttribute("nusuario", user);
 	    dao.InsertaUsuario(user);//try catch
+	    /*TODO:COOKIES
+	    Cookie c = new Cookie("emailCookie", email);
+	    c.setMaxAge(60*60*24*365*2); 
+	    c.setPath("/");
+	    response.addCookie(c);
+		*/
 		return "Tienda";
 	}
 	
@@ -142,34 +150,55 @@ public class HomeController {
 	
 	@RequestMapping(value = "/añadir", method = RequestMethod.POST)
 	public String añadir(HttpServletRequest request,Locale locale, Model model) {
-		HttpSession sesion = request.getSession(true);
+		HttpSession sesion = request.getSession(false);
+		if(sesion==null) {
+			sesion=request.getSession(true);}
 		int art= Integer.parseInt(request.getParameter("articulo"));
 		ProductosDTO articulo=daop.BuscarProducto(art);
-		if(itemsCarrito.contains(articulo)){
-			itemsCarrito.get(itemsCarrito.indexOf(articulo)).setCantidad(itemsCarrito.get(itemsCarrito.indexOf(articulo)).getCantidad()+1);
+		
+		//Se comprueba que existe una sesion para su carrito y si es null, la inicializamos y añadimos a la sesion
+		if(sesion.getAttribute("itemsCarrito")==null) {
+			List<ProductosDTO> SesionCarrito = new ArrayList<ProductosDTO>();
+			sesion.setAttribute("itemsCarrito", SesionCarrito);
+		}
+		//Aquí recogemos de la sesion el carrito si lo hubiera(o lo hayamos inicializado)
+		List<ProductosDTO> SesionCarrito =(ArrayList<ProductosDTO>) sesion.getAttribute("itemsCarrito");
+		
+		//Para simplificar y no rellenar de objetos iguales el carrito, comprobamos si contiene ese objeto 
+		//y SOLO CAMBIAMOS SU CANTIDAD (actual+1)
+		if(SesionCarrito.contains(articulo)){
+			
+			SesionCarrito.get(SesionCarrito.indexOf(articulo)).setCantidad(SesionCarrito.get(SesionCarrito.indexOf(articulo)).getCantidad()+1);
 			}else {
-		itemsCarrito.add(articulo);
-		itemsCarrito.get(itemsCarrito.indexOf(articulo)).setCantidad(itemsCarrito.get(itemsCarrito.indexOf(articulo)).getCantidad()+1);
-		sesion.setAttribute("carrito", itemsCarrito);
-		sesion.setAttribute("tam",itemsCarrito.size());
+		//En el caso de que no exista ese articulo/objeto lo añadimos y hacemos cantidad=1, entonces este objeto ya nunca entra en este else
+	    SesionCarrito.add(articulo);
+		SesionCarrito.get(SesionCarrito.indexOf(articulo)).setCantidad(SesionCarrito.get(SesionCarrito.indexOf(articulo)).getCantidad()+1);
 			}
+		//Finalmente actualizamos carrito en la sesion
+		sesion.setAttribute("itemsCarrito", SesionCarrito);
+		sesion.setAttribute("tam",SesionCarrito.size());
+		
 		return "Tienda";
 	}
 	
 	@RequestMapping(value = "/carrito", method = RequestMethod.GET)
 	public String carrito(HttpServletRequest request,Locale locale, Model model) {
 		HttpSession sesion = request.getSession(true);
-		model.addAttribute("items",itemsCarrito);
+		List<ProductosDTO> SesionCarrito=(ArrayList<ProductosDTO>)sesion.getAttribute("itemsCarrito");
+		if (SesionCarrito==null) {
+			model.addAttribute("noItems","No ha insertado ningún producto en el carrito");
+		}else {
+		model.addAttribute("items",SesionCarrito);
 		//SumaTotal;
 		int i=0;
 		float suma=0;
-		for (i=0;i<itemsCarrito.size();i++) {
-			suma=suma+itemsCarrito.get(i).getPrecio()*itemsCarrito.get(i).getCantidad();
+		for (i=0;i<SesionCarrito.size();i++) {
+			suma=suma+SesionCarrito.get(i).getPrecio()*SesionCarrito.get(i).getCantidad();
 				
 		}
 		model.addAttribute("suma", suma);	
 		//itemsCarrito.get(0);
-		
+		}
 			return "Carrito";
 	}
 	
--- Practica_1_5/src/main/webapp/WEB-INF/views/Carrito.jsp
+++ Practica_1_5/src/main/webapp/WEB-INF/views/Carrito.jsp
@@ -11,7 +11,9 @@
 ${items[0]}<br>
 ${items[1]}<br>
 ${items[2]}<br>
+${noItems}
 
+<st:if test= "${items !=null}">
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_xclick">
 <input type="hidden" name="business" value="dmg00040-facilitator@red.ujaen.es">
@@ -28,6 +30,6 @@ ${items[2]}<br>
 <input type="image" src="https://www.sandbox.paypal.com/es_ES/ES/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma rápida y segura de pagar en Internet.">
 <img alt="" border="0" src="https://www.sandbox.paypal.com/es_ES/i/scr/pixel.gif" width="1" height="1">
 </form>
-
+</st:if>
 </body>
 </html>
